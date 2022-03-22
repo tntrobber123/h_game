268de995f70d966fc456bc6543583a7873d7a6d6
Tai Falor <taifalor@gmail.com>
2022-03-21 20:57:14 -0600

    fixed stuff, game works, but no mechanics yet.

---
+++ wolf.py
@@ -0,0 +1,16 @@
+# Wolf char
+
+""" Viable Locations:
+*camp
+
+*cabin - forest-y area with a cabin in front
+*river - not many trees, but a small clearing with a medium sized river
+*river 2 - more trees, smaller river
+*forrest 1 - hugly dense forrest
+*forrest 2 - hugly dense forrest, but different
+*hill - clearing, with a field a small distance away
+*field - clearing, not much there. trees in the distance
+*abandon mine - small clearing that leads to a hill with a mine in it
+
+Viable Locations """
+loc = 'field'
